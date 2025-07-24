39b521dc2d2b3fdc156ca8cbb4266974bda4d5bb
AIML-Engineer1 <rajsrinit@gmail.com>
2025-07-24 15:50:07 -0700

    Create translator_config.py

---
+++ translator_config.py
@@ -0,0 +1,83 @@
+SENTENCE = {
+  "sent1": {
+
+        "en": "JustAccess ",
+
+        "sp": "Acceso Justo "
+
+    },
+  "sent2": {
+
+        "en": "AI Legal Aid Navigator for Underserved Communities",
+
+        "sp": "Navegador de Asistencia Legal por IA para Comunidades Desatendidas"
+
+    },
+  "sent3": {
+
+        "en": "Would you like me to search for law firms near your location?",
+
+        "sp": "¿Quieres que busque despachos de abogados cerca de tu ubicación?"
+  },
+  "sent4": {
+
+        "en": "Hi, I am a chatbot designed to help provide legal aid and advice in simple terms.",
+
+        "sp": "Hola, soy un chatbot diseñado para ayudar a brindar ayuda y asesoramiento legal en términos simples."
+  },
+  "sent5": {
+
+        "en": "Log in with Google",
+
+        "sp": "Iniciar sesión con Google"
+
+    },
+
+    "sent6": {
+
+        "en": "Log out",
+
+        "sp": "Cerrar sesión"
+
+    },
+
+    "sent7": {
+
+        "en": "Please Select a Language of Your Choice...",
+
+        "sp": "Por favor, seleccione un idioma..."
+
+    },
+
+    "sent8": {
+
+        "en": "Select Language",
+
+        "sp": "Seleccione un idioma"
+
+    },
+
+    "sent9": {
+
+        "en": "Hello there!!!!, I am a highly trained Legal Aid Navigator. You can ask me any queries about the child care.",
+
+        "sp": "¡Hola! Soy una asesora legal altamente capacitada. Pueden hacerme cualquier pregunta sobre el cuidado infantil."
+
+    },
+
+    "sent10": {
+
+        "en": "Please login to access this page",
+
+        "sp": "Por favor, inicie sesión para acceder a esta página"
+
+    },
+
+    "sent11": {
+
+        "en": "JustAccess ChatBot ",
+
+        "sp": "Chatbot de JustAccess "
+
+    }
+}
